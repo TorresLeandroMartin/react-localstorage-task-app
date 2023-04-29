e0309e81bd31970ea3e7a926ada40608aee2e40f
TorresLeandroMartin <leandromartintorres17797@gmail.com>
2023-04-29 20:18:39 -0300

    task app version 1

---
+++ src/components/container.js
@@ -0,0 +1,9 @@
+import { Children } from "react";
+
+export const Container = ({ children }) => {
+  return (
+    <div className="container p-4">
+      <div className="col-md-4 offset-md-4">{children}</div>
+    </div>
+  );
+};
